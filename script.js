568b72dd7c5610fb43cb1afaba2a2d6569ef2f07
seungyong1979 <seungyong809@gmail.com>
2025-09-02 11:32:12 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.toggle('active', i === index);
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % totalSlides;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000);
+
+showSlide(0);
